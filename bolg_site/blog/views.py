4b967f7f7c361e87a6e419ecfe640376eb38cb20
sahan <sahan.m.rajapaksha@gmail.com>
2023-12-07 11:16:52 +0530

    first commit

--- bolg_site/blog/views.py
+++ bolg_site/blog/views.py
@@ -1,3 +1,33 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse
 # Create your views here.
+
+posts = [
+    {
+        'author' : 'Sahan',
+        'title' : 'Blog Post 1',
+        'content' : 'First post Content',
+        'date_posted' : 'December 7,2023'
+    },
+    {
+        'author' : 'Sara',
+        'title' : 'Blog Post 2',
+        'content' : 'Second post Content',
+        'date_posted' : 'December 7,2023'
+    }
+
+]
+
+
+def home(request):
+    # return HttpResponse('<h1>Blog Home</h1>')
+    context = {
+        'posts' : posts
+    }
+    return render(request , 'blog/home.html', context)
+
+def log(request):
+    # return HttpResponse('<h1>This is log</h1><p>lorem</p>')
+    return render(request , 'blog/about.html')
+
+    
